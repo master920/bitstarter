e51a63cd158009bb3950111df66922acf6f97ced
Mark Nelson <mnelson920@live.com>
2013-07-22 01:22:57 +0000

    Edited web.js

--- web.js
+++ web.js
@@ -5,13 +5,13 @@ var data;
 
 app.use(express.logger());
 
-data = fs.readFileSync("index.html", "utf8");
+data = fs.readFileSync("index.html","utf8");
 buf = new Buffer(data);
 buf.write(data);
-
+var x = buf.length;
 
 app.get('/', function(request, response) {
-  response.send(buf.toString("utf8",0,28));
+  response.send(buf.toString("utf8",0,x));
 });
 
 var port = process.env.PORT || 5000;
